6a50e75afeaca989edec43a3acfa225f9968a34b
Dhrub Thakur <66480@hexaware.com>
2022-01-12 11:49:59 +0530

    Add files

--- src/Test.groovy
+++
@@ -1,8 +0,0 @@
-public class Test{
-public Test(){
-println("In contructor....");
-}
-public void displayName(String name){
-  println("From test ${name}")
-}
-}
---
+++ src/com/Test.groovy
@@ -0,0 +1,10 @@
+package com;
+
+public class TestSrc{
+public TestSrc(){
+println("In contructor....");
+}
+public void displayName(String name){
+  println("From test ${name}")
+}
+}
