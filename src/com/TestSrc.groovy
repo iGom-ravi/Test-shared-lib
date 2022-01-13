464dff7bc55aeaeee3ec367e3046cbda0437227e
iGom-ravi <95466376+iGom-ravi@users.noreply.github.com>
2022-01-13 15:00:52 +0530

    Update TestSrc.groovy

--- src/com/TestSrc.groovy
+++ src/com/TestSrc.groovy
@@ -1,10 +1,6 @@
 package com;
 
 public class TestSrc{
-public TestSrc(){
-println("In contructor....");
-}
-public String displayName(String name){
-  return "From TestSrc ${name}"
-}
+  public void PrintName(String name){
+    println("From the test ${name}")
 }
