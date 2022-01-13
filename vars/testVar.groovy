d8becb7e3d348fd6a3793d6bd4a77bc82707643f
iGom-ravi <95466376+iGom-ravi@users.noreply.github.com>
2022-01-13 16:36:22 +0530

    Update testVar.groovy

--- vars/testVar.groovy
+++ vars/testVar.groovy
@@ -2,6 +2,7 @@ import com.*;
 
   def info(String name){
     TestSrc test= new TestSrc();
+    println(test)
     println(test.displayName(name))
      println("Hello  ${name}")
     println("r u working")
